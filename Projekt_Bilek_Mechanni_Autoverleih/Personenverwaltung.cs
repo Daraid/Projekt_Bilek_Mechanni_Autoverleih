050d0e63f6170da3229ede8e7713049335d06ea9
Daraid <dawid73@hotmail.com>
2016-12-16 08:08:18 +0100

    Add project files.

---
+++ Projekt_Bilek_Mechanni_Autoverleih/Personenverwaltung.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Bilek_Mechanni_Autoverleih
+{
+    class Personenverwaltung
+    {
+        private List<Person> personen;
+        private List<Fahrzeug> angebote;
+
+        public Personenverwaltung(List<Person> personen)
+        {
+            Personen = personen;
+        }
+
+        internal List<Person> Personen
+        {
+            get
+            {
+                return personen;
+            }
+
+            set
+            {
+                personen = value;
+            }
+        }
+
+        internal List<Fahrzeug> Angebote
+        {
+            get
+            {
+                return angebote;
+            }
+
+            set
+            {
+                angebote = value;
+            }
+        }
+    }
+}
